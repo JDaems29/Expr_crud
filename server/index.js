7d53251d0688c31ae4c76203fd473d8caabcbb83
ItsJoey29 <daems.joeyy@gmail.com>
2021-10-06 23:10:11 +0200

    EOD Commit 06/10

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
-import { Express } from "express";
+import express from "express";
 import bodyParser from "body-parser";
-import { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://JoeyDaems:JoeyDaems123@exprcluster.s5d0g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Server running on ${PORT}`)});
