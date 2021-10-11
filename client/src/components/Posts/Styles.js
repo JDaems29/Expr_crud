55158b3dbbd5c8a81cbdecc2ab5ee0ff820c20a5
ItsJoey29 <daems.joeyy@gmail.com>
2021-10-11 16:22:04 +0200

    Styles added

---
+++ client/src/components/Posts/Styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core/styles";
+import useStyles from "./styles";
+
+export default makeStyles((theme) => ({
+  const classes = useStyles();
+  mainContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+  smMargin: {
+    margin: theme.spacing(1),
+  },
+  actionDiv: {
+    textAlign: "center",
+  },
+}));
--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
-import express from "express";
+import express, { Router } from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use('/posts', postRoutes);
-app.use(cors());
+app.use("/posts", postRoutes);
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-const CONNECTION_URL = "mongodb+srv://JoeyDaems:JoeyDaems123@exprcluster.s5d0g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL =
+  "mongodb+srv://JoeyDaems:JoeyDaems123@exprcluster.s5d0g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Server running on ${PORT}`)});
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:${3000}`);
+});
