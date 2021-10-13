cdf6ba0739ee3c2b6a34ae4f219cb1d79b7b5169
ItsJoey29 <daems.joeyy@gmail.com>
2021-10-13 15:36:21 +0200

    Dotenv update

--- server/index.js
+++ server/index.js
@@ -12,12 +12,14 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://JoeyDaems:JoeyDaems123@exprcluster.s5d0g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 3000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
